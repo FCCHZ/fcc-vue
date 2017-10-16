a8278352d5cf850abc45bc110b1a1dbd888bd871
huguangnan <wx-hugn@dtdream.com>
2017-10-16 20:25:50 +0800

    blog update

--- src/view/Blog/bolg.vue
+++ src/view/Blog/bolg.vue
@@ -3,7 +3,6 @@
 		<section class="search">
 			<input type="text" name='search' placeholder="请输入想要搜索的博客">
 		</section>
-		<p>{{msg}}</p>
 	</div>
 </template>
 <script>
@@ -11,7 +10,6 @@
 		name: 'blog',
 		data() {
 			return {
-				msg: "这里是 成员博客 页面"
 			}
 		}
 	}
@@ -19,7 +17,16 @@
 <style lang = 'scss' scoped>
 #blog {
 	.search {
-
+		margin: 20px auto;
+		width: 460px;
+		height: 50px;
+		input {
+			width: 420px;
+			border: 1px solid #e1e1e1;
+			height: 100%;
+			text-indent: 20px;
+			font-size: 14px;
+		}
 	}
 }
 
